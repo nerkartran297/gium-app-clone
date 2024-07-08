4429bda621596b008ab693abdc104c9e6423b620
Khanh Tran <nerkar.tran@gmail.com>
2024-07-08 18:36:03 +0700

    Nav + SideBar - Responsive

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MeetingTypeList = () => {
+    return <section className="grid">MeetingTypeList</section>;
+};
+
+export default MeetingTypeList;
---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+export const sidebarLink = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/Home.svg',
+    },{
+        label: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg',
+    },{
+        label: 'Previous',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg',
+    },{
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/Video.svg',
+    },{
+        label: 'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg',
+    },
+]
