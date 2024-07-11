c289166b6008a733bc741b77a83b56c0c45fd5e3
Khanh Tran <nerkar.tran@gmail.com>
2024-07-11 10:51:48 +0700

    Added Join Meeting

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -8,6 +8,7 @@ import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useToast } from "./ui/use-toast";
 import { Textarea } from "./ui/textarea";
 import ReactDatePicker from "react-datepicker";
+import { Input } from "./ui/input";
 
 const MeetingTypeList = () => {
     const router = useRouter();
@@ -173,6 +174,23 @@ const MeetingTypeList = () => {
                 buttonText="Start Meeting"
                 handleClick={createMeeting}
             />
+
+            <MeetingModals
+                isOpen={meetingState === "isJoiningMeeting"}
+                onClose={() => setMeetingState(undefined)}
+                title="Type the link here"
+                className="text-center"
+                buttonText="Join Meeting"
+                handleClick={() => router.push(values.link)}
+            >
+                <Input
+                    placeholder="Meeting link"
+                    className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    onChange={(e) =>
+                        setValues({ ...values, link: e.target.value })
+                    }
+                />
+            </MeetingModals>
         </section>
     );
 };
